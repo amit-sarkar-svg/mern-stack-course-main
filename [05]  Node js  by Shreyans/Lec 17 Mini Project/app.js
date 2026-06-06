41688ac7244064424f2599992a064c2edd8da591
Amit Sarkar <amitsarkar6124@gmail.com>
2026-06-06 23:13:00 +0530

    input feature added & learned multer

--- [05]  Node js  by Shreyans/Lec 17 Mini Project/app.js
+++ [05]  Node js  by Shreyans/Lec 17 Mini Project/app.js
@@ -5,14 +5,50 @@ const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static("public"));
+
+// Create uploads directory if it doesn't exist
+const uploadsDir = path.join(__dirname, "./public/images/uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadsDir);
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12, (err, bytes) => {
+      const fn = bytes.toString("hex") + path.extname(file.originalname);
+      cb(null, fn);
+    });
+  },
+});
+const upload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  console.log("File uploaded:", req.file);
+  res.json({ message: "File uploaded successfully", file: req.file });
 });
 
 app.get("/login", (req, res) => {
@@ -48,7 +84,13 @@ app.get("/edit/:id", isLoggedIn, async (req, res) => {
 });
 
 app.post("/update/:id", isLoggedIn, async (req, res) => {
-  let post = await postModel.findOneAndUpdate({ _id: req.params.id }, { content: req.body.content }, { new: true }).populate("user");
+  let post = await postModel
+    .findOneAndUpdate(
+      { _id: req.params.id },
+      { content: req.body.content },
+      { new: true },
+    )
+    .populate("user");
 
   res.redirect("/profile", { post });
 });
